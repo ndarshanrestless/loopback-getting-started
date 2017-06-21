eec885a0f2cdb33d992f2f92ba9d70a6e0d9e0b1
Firstname Lastname <your.email@autodesk.com>
2017-06-21 14:57:07 -0700

    added mocha and readme.md

--- test/test.js
+++ test/test.js
@@ -1,13 +1,20 @@
-var request = require("request"),
-    assert = require('assert'),
-    expect = require('expect'),
-    base_url = "http://localhost:3000/";
+var const request = require("request"),
+    const expect = require('expect'),
+    const base_url = "http://localhost:3000/",
+    const assert = require('chai').assert;
+
+//const request = require('supertest');
+//assert = require('assert'),
+//const sinon = require('sinon');
 
 describe("Hello World Server", function() {
   describe("GET /", function() {
     it("returns status code 200", function(done) {
       request.get(base_url, function(error, response, body) {
         expect(response.statusCode).toBe(200);
+        if(false){
+          consle.log("dint get 200 response");
+        }
         assert.equal(200, response.statusCode);
         done();
       });
